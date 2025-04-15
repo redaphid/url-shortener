3eaba372b63cdf3bd340353defbdc8848e53b621
redaphid <iam@aaronherres/com>
2025-04-14 21:46:23 -0700

    first try with code

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 /**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+ * 2cb.pw - URL Shortener
+ *  - if someone makes  GET request to /{short_url}
+ *  - check if it exists in KV
+ *  - if it does, redirect to the original URL permanently
+ *  - if it doesn't, return a 404
+ **/
+const getLongUrl = async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => {
+  const shortUrl = request.url.split("/").pop()
+  if (!shortUrl) return new Response("Not found", { status: 404 })
+  const url = await env.short_urls.get(shortUrl)
+  if (url) return Response.redirect(url, 301)
+  return new Response("Not found", { status: 404 })
+}
+
+const createShortUrl = async (request: Request, env: Env, ctx: ExecutionContext): Promise<Response> => {
+  return new Response("Not yet.", { status: 403 })
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+  async fetch(request, env, ctx): Promise<Response> {
+    if (request.method === "GET") return getLongUrl(request, env, ctx)
+    if (request.method === "POST") return createShortUrl(request, env, ctx)
+    return new Response("Not found", { status: 404 })
+  },
+} satisfies ExportedHandler<Env>
